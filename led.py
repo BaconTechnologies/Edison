922cf19c38762f85c0f27371050640f698084fd3
renatosalas <juventus_s12@live.com.mx>
2016-03-14 17:24:15 -0600

    testing grove button

--- led.py
+++ led.py
@@ -2,20 +2,22 @@ import time
 import pyupm_grove as grove
 
 # Create the Grove LED object using GPIO pin 2
-ledON = grove.GroveLed(2)
+'''ledON = grove.GroveLed(2)
 ledOFF = grove.GroveLed(6)
 
 enterZone1 = grove.GroveButton(4)
-enterZone2 = grove.GroveButton(8)
+enterZone2 = grove.GroveButton(8)'''
 
 def availableSpaces():
-	global ledON, ledOFF
+	ledON = grove.GroveLed(2)
+	ledOFF = grove.GroveLed(6)
 
 	ledOFF.off()
 	ledON.on()
 
 def notAvailableSpaces():
-	global ledON, ledOFF
+	ledON = grove.GroveLed(2)
+	ledOFF = grove.GroveLed(6)
 
 	ledON.off()
 	ledOFF.on()
