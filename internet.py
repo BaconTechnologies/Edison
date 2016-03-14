f680f1514586d472d4725e012c7bc6b512861b66
renatosalas <juventus_s12@live.com.mx>
2016-03-14 15:20:49 -0600

    testing grove button

--- internet.py
+++ internet.py
@@ -1,16 +1,18 @@
 import urllib2
 import json
 
-def request( whereAreYouGoing ):
-	url = "https://randomuser.me/api/?results=" + whereAreYouGoing
-
-	print url
+def enterZone( zoneNumber ):
+	url = "https://randomuser.me/api/?results=" + zoneNumber
 
 	html = urllib2.urlopen( url ).read()
 
 	try: js = json.loads(html)
 	except: js = None
 
+	zoneName = "(Enter) Zone " + zoneNumber
+	print zoneName
+
+	'''
 	basePath = "results"
 
 	for i in range( 0, len( js[basePath] ) ):
@@ -18,3 +20,25 @@ def request( whereAreYouGoing ):
 		password = js[basePath][i]["user"]["password"]
 
 		print (firstName + '	password: ' + password).encode('utf-8')
+	'''
+
+def exitZone( zoneNumber ):
+	url = "https://randomuser.me/api/?results=" + zoneNumber
+
+	html = urllib2.urlopen( url ).read()
+
+	try: js = json.loads(html)
+	except: js = None
+
+	zoneName = "(Exit) Zone " + zoneNumber
+	print zoneName
+
+	'''
+	basePath = "results"
+
+	for i in range( 0, len( js[basePath] ) ):
+		firstName = js[basePath][i]["user"]["email"]
+		password = js[basePath][i]["user"]["password"]
+
+		print (firstName + '	password: ' + password).encode('utf-8')
+	'''
