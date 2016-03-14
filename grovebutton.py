8e7d3676ce47a9f4d4c64b8374f1c1350f96ac0b
renatosalas <juventus_s12@live.com.mx>
2016-03-14 12:52:44 -0600

    testing grove button

--- grovebutton.py
+++ grovebutton.py
@@ -29,7 +29,7 @@ button = grove.GroveButton(8)
 # Read the input and print, waiting one second between readings
 while 1:
     print button.name(), ' value is ', button.value()
-    time.sleep(0.5)
+    time.sleep(0.8)
 
 # Delete the button object
 del button
