ecae3556b6ee64c066eb6b21e4f569765b7f4a01
renatosalas <juventus_s12@live.com.mx>
2016-03-15 12:56:54 -0600

    Testing creating files

---
+++ creatingFile.py
@@ -0,0 +1,17 @@
+import sys
+
+def write():
+    print('Creating new text file') 
+
+    name = input('Enter name of text file: ')+'.txt'  # Name of text file coerced with +.txt
+
+    try:
+        file = open(name,'w')   # Trying to create a new file or open one
+        file.write("Some text")
+        file.close()
+
+    except:
+        print('Something went wrong! Can\'t tell what?')
+        sys.exit(0) # quit Python
+
+write()
