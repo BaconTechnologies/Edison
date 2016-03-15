da5a71447b77f4f12137162e00d02603899368a8
renatosalas <juventus_s12@live.com.mx>
2016-03-14 18:50:04 -0600

    testing grove button

--- internet.py
+++ internet.py
@@ -2,11 +2,13 @@ import urllib2
 import json
 
 from led import *
+from lcd import *
 
 # Set max capacity 
 limitSpace = 10
 zones = [[], [], []]
 
+# Init max capacity of each zone
 for i in range(0, len(zones)):
 	zones[i] = limitSpace
 
@@ -15,6 +17,7 @@ def enterZone( zoneNumber ):
 
 	url = "https://randomuser.me/api/?results=" + zoneNumber
 
+	# Cast zoneNumber to int (get from REST API)
 	zoneNumber = int(zoneNumber)
 
 	'''
@@ -32,24 +35,28 @@ def enterZone( zoneNumber ):
 		print (firstName + '	password: ' + password).encode('utf-8')
 	'''
 
+	# Capacity its in valid range
 	if zones[zoneNumber] > 0 and zones[zoneNumber] <= limitSpace:
 		zones[zoneNumber] = zones[zoneNumber] - 1
 		availableSpaces()
 	else:
+		# Parking its full
 		print 'No more space'
 		notAvailableSpaces()
 
+	# Check if parking its full
 	if zones[zoneNumber] == 0:
 		notAvailableSpaces()
 
 	zoneName = "Zone " + str(zoneNumber) + "			Available: " + str(zones[zoneNumber])
-	print zoneName
+	showInScreen( zoneName )
 
 def exitZone( zoneNumber ):
 	global zones
 
 	url = "https://randomuser.me/api/?results=" + zoneNumber
 
+	# Cast zoneNumber to int (get from REST API)
 	zoneNumber = int(zoneNumber)
 
 	'''
@@ -67,12 +74,14 @@ def exitZone( zoneNumber ):
 		print (firstName + '	password: ' + password).encode('utf-8')
 	'''
 
+	# Capacity its in valid range
 	if zones[zoneNumber] >= 0 and zones[zoneNumber] < limitSpace:
 		zones[zoneNumber] = zones[zoneNumber] + 1
 		availableSpaces()
 	else:
+		#Capacity out of range
 		print 'Upss;'
 		notAvailableSpaces()
 
 	zoneName = "Zone " + str(zoneNumber) + "			Available: " + str(zones[zoneNumber])
-	print zoneName
+	showInScreen( zoneName )
