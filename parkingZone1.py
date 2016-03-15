1718361dffd7b1a777b070dda636e43bac29eabf
renatosalas <juventus_s12@live.com.mx>
2016-03-14 19:20:14 -0600

    testing grove button

--- parkingZone1.py
+++ parkingZone1.py
@@ -6,11 +6,9 @@ from internet import *
 
 # Create button sensor in D8 && D4
 enterZone1 = grove.GroveButton(4)
-enterZone2 = grove.GroveButton(8)
 
 # Create touch sensor in D7 && D3
 exitZone1 = ttp223.TTP223(3)
-exitZone2 = ttp223.TTP223(7)
 
 # Read the input and print, waiting one second between readings
 while 1:
@@ -18,24 +16,14 @@ while 1:
 		zoneNumber = "1"
 		enterZone( zoneNumber )
 
-	if enterZone2.value() :
-		zoneNumber = "2"
-		enterZone( zoneNumber )
-
 	if exitZone1.value() :
 		zoneNumber = "1"
-		exitZone( zoneNumber )
-	
-	if exitZone2.value() :
-		zoneNumber = "2"
 		exitZone( zoneNumber )
 	
 	time.sleep(1)
 
 # Delete the button object
 del enterZone1
-del enterZone2
 
 # Delete the touch sensor object
 del exitZone1
-del exitZone2
